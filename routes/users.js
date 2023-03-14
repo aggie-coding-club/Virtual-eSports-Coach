f8230ba64d0e4892c4fc26dabb03f3dab5f77174
chris <116124688+kiyoyoneko@users.noreply.github.com>
2023-03-14 04:24:31 -0500

    revert

--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,19 @@
 const bodyParser = require('body-parser');
 const router = require('express').Router();
 let User = require('../schemas/users');
+const express = require('express');
+const sl = require('serverless-http');
+const app = express();
 
-router.route('/').get((req, res) => {
+
+app.use(bodyParser);
+app.get('/', (req, res) => {
+  const newValue = updateDatabase(res.body);
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
-router.route('/add').post((req, res) => {
-  const username = req.body.username;
 
-  const newUser = new User({username});
 
-  newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
 
-router.route('/:id').get((req, res) => {
-    User.findById(req.params.id)
-      .then(users => res.json(users))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-
-  
-  router.route('/:id').delete((req, res) => {
-    User.findByIdAndDelete(req.params.id)
-      .then(() => res.json('User deleted.'))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-  
-  router.route('/update/:id').post((req, res) => {
-    User.findById(req.params.id)
-      .then(user => {
-        user.username = req.body.username;
-        user.save()
-          .then(() => res.json('User updated!'))
-          .catch(err => res.status(400).json('Error: ' + err));
-      })
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-
-module.exports = router;
+module.exports = sl(router);
