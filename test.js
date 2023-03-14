d521b180492f62cf0712affe24c099453c29011a
chris <116124688+kiyoyoneko@users.noreply.github.com>
2023-03-14 05:12:25 -0500

    revert4

--- test.js
+++ test.js
@@ -16,8 +16,6 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-
+app.use('/server', router);  // path must route to lambda
 module.exports = app;
 module.exports.handler = serverless(app);
