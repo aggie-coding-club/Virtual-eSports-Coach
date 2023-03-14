24517f0779b2c252d7fa7c500bbf259094f41528
chris <116124688+kiyoyoneko@users.noreply.github.com>
2023-03-14 03:34:08 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,26 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 //import test from './schemas/users.js';
-const express = require('express');
-const server = express();
-const cors = require('cors')
-const PORT = process.env.PORT || 8080;
-const mongoose = require('mongoose');
-require('dotenv/config')
-mongoose.connect(process.env.MONGODB_URI);
-const connection = mongoose.connection;
-connection.once('open',()=>{console.log('mongo connected!')})
-console.log(process.env.MONGODB_URI)
-const usersRouter = require('./../../routes/users')
-server.use('/users',usersRouter)
 
-server.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-server.use(cors())
-server.use(express.json());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
