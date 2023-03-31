344ec0a98d777f90067cc17cc3a0b4fc6b4fbd64
kiyoyoneko <kiyoedm@gmail.com>
2023-03-30 19:38:49 -0500

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Home } from "./pages";
-import {Register} from "./pages"
+import { Home,Register,ToS,Privacy } from "./pages";
+import { Navbar } from './components';
 /*
 When it's time, we can do:
 import { Home, Page1, Page2, Page 3 } from "./pages";
@@ -12,13 +12,23 @@ import { Home, Page1, Page2, Page 3 } from "./pages";
 const App = () => {
     return (
     <Router>
+      <div className="gradient__bg">
+      <Navbar />
+      </div>
+      <div className="gradient__bg">
       <Routes>
         <Route path="/" /*element={<Layout />}*/>
           <Route index element={<Home />} />
           <Route path="aboutus" element={<Home />} />
           <Route path="register" element={<Register />} />
+          <Route path="tos" element={<ToS />} />
+          <Route path="privacy" element={<Privacy />} />
         </Route>
       </Routes>
+      </div>
+      {/*<div className="footer">
+      <Footer />
+    </div>*/}
     </Router>
     )
 }
