158e183ffceece2df3a18ba63fdf5a615c2f0476
Thomas <thomas8483828180@gmail.com>
2023-04-04 03:32:15 -0500

    More improvements

--- client/src/components/hometitle/hometitle.jsx
+++ client/src/components/hometitle/hometitle.jsx
@@ -11,9 +11,9 @@ const HomeTitle = () => {
         rotate: [0, 360],
       });
     return (
-        <div ref={glitch.ref}className="header section__padding" id="home" style={{marginTop : -950, paddingBottom:1000}}>
+        <div ref={glitch.ref}className="hometitle section__padding" id="home" style={{marginTop : -950, paddingBottom:1000}}>
             <Parallax speed={10}>
-            <div className="header-content" >
+            <div className="hometitle-content" >
 
                 {/* Big gradient text */}
                 {/*<GlitchClip duration={"2s"}>*/}
@@ -23,7 +23,7 @@ const HomeTitle = () => {
                 <p>The primary goal of Virtual eSports Coach is to serve as a website that helps gamers improve their skills in various popular competitive shooter titles, such as VALORANT, Overwatch 2, and more. First, gamers will enter their username, platform, and other information required to identify them. Then, the program will retrieve their statistics, determine which skills need the most improvement, and offer advice and mentorship. By providing customized training, practice routines, and other guidance for each client, Virtual eSports Coach will allow even casual gamers to make significant progress and advance to the next level of gaming.</p>
 
                 {/* Username input searchbar */}
-                <div className="vec__header-username__input">
+                <div className="hometitle-username__input">
                     <input type="text" placeholder="Enter your Riot ID"/>
 
                     {/* Enter button */}
