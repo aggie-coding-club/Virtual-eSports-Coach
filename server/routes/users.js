99a02cd6866abb1c7ad3f44e2530e675551b5681
kiyoyoneko <kiyoedm@gmail.com>
2023-04-08 03:33:07 -0500

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,6 +6,7 @@ router.route('/').get((req, res) => {
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
+  console.log('get')
 });
 router.route('/add').post((req, res) => {
   const username = req.body.username;
