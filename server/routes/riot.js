f6edb7f25b4afe4b4476d7744ea0f73b46b3bf6e
chris <116124688+kiyoyoneko@users.noreply.github.com>
2023-04-14 20:22:56 -0500

    Update riot.js

--- server/routes/riot.js
+++ server/routes/riot.js
@@ -5,7 +5,7 @@ const axios = require('axios')
     axios.get(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${req.body.user}/${req.body.tag}?api_key=RGAPI-663a7a7a-8389-473f-b10a-5681c7882f0a`)
     .then((response) => {
       res.send(response);
-    });
+    }).catch(err => res.status(400).json('Error: ' + err));
     
   });
 module.exports = router;
