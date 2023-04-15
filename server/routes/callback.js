9c71d3a7adcb0eb4b366c3438d680f0af7656a4c
chris <116124688+kiyoyoneko@users.noreply.github.com>
2023-04-14 20:02:59 -0500

    Update callback.js

--- server/routes/callback.js
+++ server/routes/callback.js
@@ -1,11 +1,5 @@
 const router = require('express').Router();
 const express = require('express')
-router.use(express.json());
-router.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
   router.route('/test').post((req, res) => {
     console.log(req.body)
     res.send(req.body);
