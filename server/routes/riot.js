20f54a4a32cc65489c576ed7fee48ad60afccb1b
kiyoyoneko <kiyoedm@gmail.com>
2023-04-15 00:44:24 -0500

    Update riot.js

--- server/routes/riot.js
+++ server/routes/riot.js
@@ -4,7 +4,7 @@ const axios = require('axios')
     axios.get(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${req.body.user}/${req.body.tag}?api_key=RGAPI-663a7a7a-8389-473f-b10a-5681c7882f0a`)
     .then((response) => {
       console.log(response.data.puuid)
-      axios.get(`https://na1.api.riotgames.com/val/match/v1/matchlists/by-puuid/${response.data.puuid}?api_key=RGAPI-663a7a7a-8389-473f-b10a-5681c7882f0a`)
+      axios.get(`https://na.api.riotgames.com/val/match/v1/matchlists/by-puuid/${response.data.puuid}?api_key=RGAPI-663a7a7a-8389-473f-b10a-5681c7882f0a`)
     .then((response) => {
       res.send(response.data);
     }).catch(err => res.status(400).json('Error: ' + err));
