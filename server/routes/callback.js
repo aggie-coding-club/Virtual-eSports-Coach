ffc4b5a1f38bbae7a0c28dd80bf5718a2ef88f77
chris <116124688+kiyoyoneko@users.noreply.github.com>
2023-04-14 19:16:59 -0500

    aa

--- server/routes/callback.js
+++ server/routes/callback.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 
   router.route('/test').post((req, res) => {
-    res.send(JSON.stringify(req.body));
-    // handle the data here
-    res.send('Data received');
+    console.log(req.body)
+    res.send(req.body);
   });
   router.route('/get').get((req, res) => {
     console.log('get');
