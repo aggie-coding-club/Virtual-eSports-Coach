9380c3c11d23cfba40476c86cdff9e5378eb4b53
kiyoyoneko <kiyoedm@gmail.com>
2023-04-15 00:55:26 -0500

    Update riot.js

--- server/routes/riot.js
+++ server/routes/riot.js
@@ -3,11 +3,19 @@ const axios = require('axios')
   router.route('/puuid').post((req, res) => {
     axios.get(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${req.body.user}/${req.body.tag}?api_key=RGAPI-663a7a7a-8389-473f-b10a-5681c7882f0a`)
     .then((response) => {
-      console.log(response.data.puuid)
-      axios.get(`https://na.api.riotgames.com/val/match/v1/matchlists/by-puuid/${response.data.puuid}?api_key=RGAPI-663a7a7a-8389-473f-b10a-5681c7882f0a`)
+      res.send(response.data);
+    }).catch(err => res.status(400).json('Error: ' + err));
+  });
+  router.route('/matches/:puuid').get((req, res) => {
+    axios.get(`https://na.api.riotgames.com/val/match/v1/matchlists/by-puuid/${req.params.puuid}?api_key=RGAPI-663a7a7a-8389-473f-b10a-5681c7882f0a`)
     .then((response) => {
       res.send(response.data);
     }).catch(err => res.status(400).json('Error: ' + err));
+  });
+  router.route('/match/:matchid').get((req, res) => {
+    axios.get(`https://na.api.riotgames.com/val/match/v1/matches/${req.params.matchid}?api_key=RGAPI-663a7a7a-8389-473f-b10a-5681c7882f0a`)
+    .then((response) => {
+      res.send(response.data);
     }).catch(err => res.status(400).json('Error: ' + err));
   });
 module.exports = router;
