ffaab7b12ce95aba5bbfe681cecdb14ab15074c3
kiyoyoneko <kiyoedm@gmail.com>
2023-04-24 03:58:30 -0500

    glb compress

--- client/src/components/skybox/skybox.jsx
+++ client/src/components/skybox/skybox.jsx
@@ -7,7 +7,7 @@ import {
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 const Skybox = () => {
-  const gltf = useLoader(GLTFLoader, './skybox.glb')
+  const gltf = useLoader(GLTFLoader, './skybox-v2.glb')
   return <primitive object={gltf.scene} scale={3}/>
 };
 
