0e758a7d55d03087d40a4c67bacbc215aef1aa37
chreamy <chrislinkou@gmail.com>
2023-10-15 00:38:26 -0500

    Update analyze_match.py

--- data analysis/analyze_match.py
+++ data analysis/analyze_match.py
@@ -1,10 +1,10 @@
 import pandas as pd
+import requests
 import json
 
-f = open('data.json')
-
-data = json.load(f)
+match_id = "b879593f-9ab2-4ada-915c-bae2fb24da62"
+url = "https://na.api.riotgames.com/val/match/v1/matches/"+match_id+"?api_key=RGAPI-663a7a7a-8389-473f-b10a-5681c7882f0a"
+data = requests.get(url).json()
 for d in data:
     print(d)
 print(data)
-f.close()
